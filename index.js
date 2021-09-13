bb89299f184d0deed7cb3a83bb9c35e32585d66c
Enerhim2 <himsharma9122006@gmail.com>
2021-09-13 15:58:51 +0530

    Basic Setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 3000;
 
 app.use(express.static("src"))
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
 
 app.listen(8080, () => {
     console.log(`App is running on port: ${PORT}`)
